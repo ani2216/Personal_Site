3234a4d29c1f498e2b723987939aff548916295d
aniket <ani22ket16@gmail.com>
2025-07-02 03:46:40 +0530

    Moved backend files to root

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://personal-site-flame-eight.vercel.app'],
+  origin: ['http://localhost:5173', 'https://aniketdwivedi.vercel.app/'],
   method: "GET,POST,PUT,DELETE,PATCH,HEAD",
   credentials:true
 }
